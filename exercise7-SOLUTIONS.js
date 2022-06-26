427b5bde2b95c7069ca5558b3770b1504d950d03
afroditi chintipatoglou <afxintip@hotmail.com>
2022-06-26 23:29:32 +0300

    exercise for fake fb

---
+++ exercise7-SOLUTIONS.js
@@ -0,0 +1,48 @@
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+{
+	username: "andrei",
+	password: "supersecret"
+}
+
+// 2. Create an array which contains the object you have made above and name the array "database"
+var database = [
+	{
+		username: "andrei",
+		password: "secret"
+	}
+	{
+		username: "coco",
+		password: "123"
+	}
+	{
+		username: "mel",
+		password: "111"
+	}
+];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [
+	{
+		username: "Bobby",
+		timeline: "So tired from all that learning!"
+	},
+	{
+		username: "Sally",
+		timeline: "Javascript is sooooo cool!"
+	},
+	{
+		username: "Mitch",
+		timeline: "Javascript is preeetyy cool!"
+	}
+];
+
+function signIn(username,password){
+	if (userValid)
+}
+
+var userPrompt = prompt('What is your username?');
+var passPrompt= prompt('What is your password?');
+
+signIn (userPrompt,passPrompt);
